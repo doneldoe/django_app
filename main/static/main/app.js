e289e815d19fcf72f70f9214f672df4a11e0ae90
doneldoe <doneldoe@gmail.com>
2021-06-19 20:02:33 +0500

    minor improvements

--- main/static/main/app.js
+++ main/static/main/app.js
@@ -57,7 +57,7 @@ Webcam.set({
 });
 
 function open_passport_cam() {
-    open_passport.className += "d-none"
+    open_passport.className += " d-none"
     passport_screenshot.classList.remove("d-none")
     passport_camera.classList.remove("d-none")
     Webcam.attach('#passport_camera');
@@ -65,7 +65,7 @@ function open_passport_cam() {
 
 function take_passport_snapshot() {
     Webcam.freeze()
-    passport_screenshot.className += "d-none"
+    passport_screenshot.className += " d-none"
     redo_passpport.classList.remove("d-none")
     close_passport.classList.remove("d-none")
 }
@@ -73,15 +73,15 @@ function take_passport_snapshot() {
 function retake_passport_snapshot() {
     Webcam.unfreeze()
     passport_screenshot.classList.remove("d-none")
-    redo_passpport.className += "d-none"
-    close_passport.className += "d-none"
+    redo_passpport.className += " d-none"
+    close_passport.className += " d-none"
 }
 
 function save_passport() {
     face_wrap.classList.remove("d-none")
-    close_passport.className += "d-none"
-    redo_passpport.className += "d-none"
-    passport_camera.className += "d-none"
+    close_passport.className += " d-none"
+    redo_passpport.className += " d-none"
+    passport_camera.className += " d-none"
     passport_result.classList.remove("d-none")
     Webcam.snap(function (data_uri) {
         passport_result.innerHTML = '<img src="' + data_uri + '"/>';
@@ -90,7 +90,7 @@ function save_passport() {
 }
 
 function open_face_cam() {
-    open_face.className += "d-none"
+    open_face.className += " d-none"
     face_screenshot.classList.remove("d-none")
     face_camera.classList.remove("d-none")
     Webcam.attach('#face_camera');
@@ -98,7 +98,7 @@ function open_face_cam() {
 
 function take_face_snapshot() {
     Webcam.freeze();
-    face_screenshot.className += "d-none"
+    face_screenshot.className += " d-none"
     redo_face.classList.remove("d-none")
     close_face.classList.remove("d-none")
 }
@@ -106,14 +106,14 @@ function take_face_snapshot() {
 function retake_face_snapshot() {
     Webcam.unfreeze();
     face_screenshot.classList.remove("d-none")
-    redo_face.className += "d-none"
-    close_face.className += "d-none"
+    redo_face.className += " d-none"
+    close_face.className += " d-none"
 }
 
 function save_face() {
-    close_face.className += "d-none"
-    redo_face.className += "d-none"
-    face_camera.className += "d-none"
+    close_face.className += " d-none"
+    redo_face.className += " d-none"
+    face_camera.className += " d-none"
     face_result.classList.remove("d-none")
     Webcam.snap(function (data_uri) {
         face_result.innerHTML = '<img src="' + data_uri + '"/>';
